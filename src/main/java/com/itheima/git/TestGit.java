282d8234b5f455a682b54d5f5f203dcf88f6e423
fuzhiyou <18289387963@163.com>
2020-07-12 16:30:40 +0800

    git3

--- src/main/java/com/itheima/git/TestGit.java
+++ src/main/java/com/itheima/git/TestGit.java
@@ -4,5 +4,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("git3");
     }
 }
